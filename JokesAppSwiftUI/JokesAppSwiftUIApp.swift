19bcb793b51681842e39814f81c35774cf2b894a
mrtoktm <mrtoktm9@gmail.com>
2024-07-20 19:15:58 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Mert Öktem on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
